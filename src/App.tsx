9bff261ae7759f223221d4b2fadbdffb6b099ba9
Octavio Arruda <octavio@maxiprod.com.br>
2020-07-08 00:27:09 -0300

    added styles and Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
+import Layout from './components/Layouts';
 
 function App() {
   return (
     <>
-    <h1>RocketSeat</h1>
+    <Layout />
 
     <GlobalStyles />
     </>
