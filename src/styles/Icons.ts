078e2690bf0ce73ae9b95cc83ab92187f4cf0216
Octavio Arruda <octavio@maxiprod.com.br>
2020-07-10 01:06:08 -0300

    Added component styles for menu icons

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -7,6 +7,7 @@ export {
     LocationOn,
     Search,
 } from 'styled-icons/material-outlined';
+
 export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
-export { Chat } from 'styled-icons/heroicons-outline';
+export { Chat } from 'styled-icons/heroicons-outline'; 
